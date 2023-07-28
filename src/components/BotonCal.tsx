9e02cf22e126cbef7ff23edbde1f963a4938c134
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-07-28 14:43:25 -0300

    agregando estilados

--- src/components/BotonCal.tsx
+++ src/components/BotonCal.tsx
@@ -3,13 +3,19 @@ import {StyleSheet, Text, View} from 'react-native';
 
 interface Props {
   texto: string;
-  color: string;
+  color?: string;
 }
 
 export const BotonCal = ({texto, color = '#2d2d2d'}: Props) => {
   return (
     <View style={{...styles.boton, backgroundColor: color}}>
-      <Text style={styles.botonTexto}>{texto}</Text>
+      <Text
+        style={{
+          ...styles.botonTexto,
+          color: color === '#9b9b9b' ? 'black' : 'white',
+        }}>
+        {texto}
+      </Text>
     </View>
   );
 };
--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -8,12 +8,40 @@ export const CalculadoraScreen = () => {
     <View style={styles.calculadoraContainer}>
       <Text style={styles.resultadoPequeno}>1,500.00</Text>
       <Text style={styles.resultado}>1,500.00</Text>
+      {/* Boton  */}
       <View style={styles.fila}>
-        {/* Boron  */}
+        {/* fila de Botone  */}
         <BotonCal texto="C" color="#9b9b9b" />
         <BotonCal texto="+/-" color="#9b9b9b" />
         <BotonCal texto="del" color="#9b9b9b" />
         <BotonCal texto="/" color="#ff9427" />
+      </View>
+      <View style={styles.fila}>
+        {/* fila de Botone  */}
+        <BotonCal texto="7" />
+        <BotonCal texto="8" />
+        <BotonCal texto="9" />
+        <BotonCal texto="x" color="#ff9427" />
+      </View>
+      <View style={styles.fila}>
+        {/* fila de Botone  */}
+        <BotonCal texto="4" />
+        <BotonCal texto="5" />
+        <BotonCal texto="6" />
+        <BotonCal texto="-" color="#ff9427" />
+      </View>
+      <View style={styles.fila}>
+        {/* fila de Botone  */}
+        <BotonCal texto="1" />
+        <BotonCal texto="2" />
+        <BotonCal texto="3" />
+        <BotonCal texto="+" color="#ff9427" />
+      </View>
+      <View style={styles.fila}>
+        {/* fila de Botone  */}
+        <BotonCal texto="0" />
+        <BotonCal texto="." />
+        <BotonCal texto="=" color="#ff9427" />
       </View>
     </View>
   );
